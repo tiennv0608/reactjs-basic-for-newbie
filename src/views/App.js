b77e4bbc07003b677051b73bac38234cf850b548
James Nguyen <nv.tien1@samsung.com>
2022-09-05 08:55:37 +0700

    create component example

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import MyComponent from "./example/MyComponent";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent />
       </header>
     </div>
   );
