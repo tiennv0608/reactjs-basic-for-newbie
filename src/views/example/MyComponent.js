0cf4d55f3540c6313b18e631d597713b5089f26c
James Nguyen <nv.tien1@samsung.com>
2022-09-05 09:22:08 +0700

    done state basic

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "James",
+    channel: "none",
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
-    return <div>Hello My Component James</div>;
+    return (
+      <>
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          My name is {this.state.name}
+        </div>
+        <div className="second">My channel is : {this.state.channel}</div>
+      </>
+    );
   }
 }
 
