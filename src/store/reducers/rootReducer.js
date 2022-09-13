f41d3bb15de1267fa359aedca3abd161e19df5e7
James Nguyen <nv.tien1@samsung.com>
2022-09-13 14:38:04 +0700

    using redux to add and delete user

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,31 @@ const initState = {
   users: [
     { id: 1, name: "James" },
     { id: 2, name: "Tien" },
+    { id: 3, name: "Viet Tien" },
   ],
 };
 
 const rootReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "DELETE_USER":
+      console.log(">>> run into delete user: ", action);
+      let users = state.users;
+      users = users.filter((item) => item.id !== action.payload.id);
+
+      return {
+        ...state,
+        users,
+      };
+    case "ADD_USER":
+      let id = Math.floor(Math.random() * 10000);
+      let user = { id: id, name: `random - ${id}` };
+      return {
+        ...state,
+        users: [...state.users, user],
+      };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
 import logo from "../../assets/images/background1.png";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
   componentDidMount() {
@@ -10,8 +11,20 @@ class Home extends React.Component {
     // }, 3000);
   }
 
+  handleDeleteUser = (user) => {
+    console.log(">>>check user delete: ", user);
+    this.props.deleteUserRedux(user);
+  };
+
+  handleCreateUser = () => {
+    this.props.addUserRedux();
+  };
+
   render() {
-    console.log(">>>check props: ", this.props);
+    console.log(">>>check props redux: ", this.props.dataRedux);
+
+    let listUsers = this.props.dataRedux;
+
     return (
       <>
         <div>Hello world from homepage with James</div>
@@ -22,9 +35,36 @@ class Home extends React.Component {
             alt=""
           />
         </div>
+
+        <div>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name}
+                  &nbsp;
+                  <span onClick={() => this.handleDeleteUser(item)}>x</span>
+                </div>
+              );
+            })}
+          <button onClick={() => this.handleCreateUser()}>+</button>
+        </div>
       </>
     );
   }
 }
 
-export default Color(Home);
+const mapStateToProps = (state) => {
+  return { dataRedux: state.users };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteUserRedux: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+    addUserRedux: () => dispatch({ type: "ADD_USER" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
